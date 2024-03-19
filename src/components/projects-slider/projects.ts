e7d3f02a3377f51fdeb5c079d00b07ad0706bc05
JuniorLevel <work.junior@mail.ru>
2024-03-19 15:11:52 +0300

    Added new project

--- src/components/projects-slider/projects.ts
+++ src/components/projects-slider/projects.ts
@@ -3,20 +3,27 @@ import 'interfaces/interfaces.ts';
 export const data: IProject[] = [
   {
     id: 1,
+    name: 'Cloud App',
+    description: 'Облачное хранилище на основе NodeJS/ExpressJS.',
+    picture: '/images/projects/cloud-app.png',
+    url: 'https://cloud-app.netlify.app/',
+  },
+  {
+    id: 2,
     name: 'Landing Prime Car',
     description: 'Одностраничный сайт по продаже машин',
     picture: '/images/projects/prime-car.png',
     url: 'https://boisterous-cranachan-8377fa.netlify.app/',
   },
   {
-    id: 2,
+    id: 3,
     name: 'Qpick Store App E-commerce',
     description: 'Интернет магазин на основе API Platzi Fake Store',
     picture: '/images/projects/qpick-store.png',
     url: 'https://zesty-sprite-912bbd.netlify.app/',
   },
   {
-    id: 3,
+    id: 4,
     name: 'Hacker News App',
     description: 'Новостное приложение на основе API Hacker News App',
     picture: '/images/projects/hacker-news.png',
