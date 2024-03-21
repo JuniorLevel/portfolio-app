68a8bf8a5433757400936bafdaa518e44afb3c3a
JuniorLevel <work.junior@mail.ru>
2024-03-21 22:10:07 +0300

    21.03.2024

--- src/pages/stack/stack.ts
+++ src/pages/stack/stack.ts
@@ -4,7 +4,7 @@ export const stack: IStack[] = [
   { id: 1, data: 'Html' },
   { id: 2, data: 'CSS(SCSS, Tailwindcss)' },
   { id: 3, data: 'JavaScript' },
-  { id: 4, data: 'React JS' },
-  { id: 5, data: 'State management - Redux Toolkit' },
+  { id: 4, data: 'React JS/TS' },
+  { id: 5, data: 'State management - Redux Toolkit, Zustand' },
   { id: 6, data: 'Контроль версий - Git' },
 ];
